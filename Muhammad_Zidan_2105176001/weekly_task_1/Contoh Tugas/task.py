808defde4c9d96c9981550d471252009584edb51
Muhammad Zidan <muhammadzidan13.mz@gmail.com>
2024-03-05 17:09:25 +0800

    tugas pertama

---
+++ Muhammad_Zidan_2105176001/weekly_task_1/Contoh Tugas/task.py
@@ -0,0 +1,22 @@
+class Mahasiswa:
+    def __init__(self, nama, kelas, prodi, fakultas, tempat_tinggal, asal):
+        self.nama = nama
+        self.kelas = kelas
+        self.prodi = prodi
+        self.fakultas = fakultas
+        self.tempat_tinggal = tempat_tinggal
+        self.asal = asal
+
+    def info(self):
+        print("Nama:", self.nama)
+        print("Kelas:", self.kelas)
+        print("Prodi:", self.prodi)
+        print("Fakultas:", self.fakultas)
+        print("Tempat Tinggal:", self.tempat_tinggal)
+        print("Asal:", self.asal)
+
+# Contoh penggunaan
+mhs1 = Mahasiswa("Muhammad Zidan", "A", "Pendidikan Komputer", "FKIP", "Samarinda", "Samarinda")
+mhs1.info()
+
+
